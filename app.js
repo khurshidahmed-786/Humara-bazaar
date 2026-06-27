6228d13d0acf8f6621b9fefdc3d7634d6425a8b2
khurshidahmed-786 <founderhumarabazaar@gmail.com>
2026-06-27 12:14:08 +0530

    app.js

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
-function saveShop(data){
+// HMARA BAZAAR GLOBAL APP FILE
+console.log("Hmara Bazaar App Loaded");
 
+// save data
+function saveData(key,value){
 localStorage.setItem(
-"shopData",
-JSON.stringify(data)
+key,
+JSON.stringify(value)
 );
+}
+
+// load data
+function loadData(key){
+
+let data=
+localStorage.getItem(key);
+
+if(data){
+
+return JSON.parse(data);
 
 }
 
-function getShop(){
-
-let shop=
-localStorage.getItem(
-"shopData"
-);
-
-if(shop){
-
-return JSON.parse(shop);
-
-}
-
-return {};
-
-}
-
-function updateShop(key,value){
-
-let shop=getShop();
-
-shop[key]=value;
-
-saveShop(shop);
+return null;
 
 }
