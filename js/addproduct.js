6259b2606146f95a820ac6bb2be82b0258b0e91f
khurshidahmed-786 <founderhumarabazaar@gmail.com>
2026-07-12 22:56:19 +0530

    addproduct.js

---
+++ js/addproduct.js
@@ -0,0 +1,45 @@
+function publishProduct(){
+
+    const currentShop = getCurrentShop();
+
+    if(!currentShop){
+
+        alert("Please create a shop first.");
+
+        return;
+
+    }
+
+    const product={
+
+        id:Date.now(),
+
+        shopId:currentShop.id,
+
+        name:document.getElementById("productName").value,
+
+        price:Number(
+
+            document.getElementById("productPrice").value
+
+        ),
+
+        description:document.getElementById("productDescription").value,
+
+        emoji:document.getElementById("productEmoji").value || "📦",
+
+        featured:true,
+
+        active:true,
+
+        createdAt:new Date().toISOString()
+
+    };
+
+    saveProduct(product);
+
+    alert("Product Published Successfully!");
+
+    window.location.href="dashboard.html";
+
+}
