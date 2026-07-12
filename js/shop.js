49efc2b4e5dacf97ceceeeaaa746fb592d010dd3
khurshidahmed-786 <founderhumarabazaar@gmail.com>
2026-07-12 18:15:47 +0530

    shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,19 +1,65 @@
+/* ===========================
+   SHOP MODULE
+=========================== */
+
 function createShop(shop){
+
+    let shops = getShops();
+
+    shop.id = Date.now();
+
+    shop.createdAt = new Date().toISOString();
+
+    shops.push(shop);
+
+    saveData(DB.SHOPS, shops);
+
+    return shop;
 
 }
 
 function getShop(id){
 
+    let shops = getShops();
+
+    return shops.find(s => s.id == id);
+
 }
 
 function getAllShops(){
 
+    return getShops();
+
 }
 
-function updateShop(shop){
+function updateShop(updatedShop){
+
+    let shops = getShops();
+
+    shops = shops.map(shop =>
+
+        shop.id == updatedShop.id
+
+        ? updatedShop
+
+        : shop
+
+    );
+
+    saveData(DB.SHOPS, shops);
 
 }
 
 function deleteShop(id){
+
+    let shops = getShops();
+
+    shops = shops.filter(
+
+        shop => shop.id != id
+
+    );
+
+    saveData(DB.SHOPS, shops);
 
 }
