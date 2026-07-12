d44f5278941f588b37daeaf508ba7ffd5ac091a0
khurshidahmed-786 <founderhumarabazaar@gmail.com>
2026-07-12 22:23:32 +0530

    product.js

--- js/product.js
+++ js/product.js
@@ -1,1 +1,45 @@
+function getProducts(){
 
+    return getData(DB.PRODUCTS) || [];
+
+}
+
+function saveProduct(product){
+
+    let products = getProducts();
+
+    products.push(product);
+
+    saveData(DB.PRODUCTS, products);
+
+}
+
+function getProductById(id){
+
+    return getProducts().find(
+
+        p => p.id == id
+
+    );
+
+}
+
+function getProductsByShop(shopId){
+
+    return getProducts().filter(
+
+        p => p.shopId == shopId
+
+    );
+
+}
+
+function getFeaturedProducts(){
+
+    return getProducts().filter(
+
+        p => p.featured === true && p.active === true
+
+    );
+
+}
