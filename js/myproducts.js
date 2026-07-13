dc43df37d78844ff8ff93e8d2f9eeb53f094fbf2
khurshidahmed-786 <founderhumarabazaar@gmail.com>
2026-07-13 23:36:31 +0530

    myproducts.js

--- js/myproducts.js
+++ js/myproducts.js
@@ -1,1 +1,37 @@
+const app = document.getElementById(
 
+"productsApp"
+
+);
+
+app.innerHTML = `
+
+<div class="container">
+
+<div class="header">
+
+<div class="title">
+
+My Products
+
+</div>
+
+<div
+class="addBtn"
+onclick="location.href='addproduct.html'">
+
+➕ Add Product
+
+</div>
+
+</div>
+
+<div
+class="products"
+id="productList">
+
+</div>
+
+</div>
+
+`;
