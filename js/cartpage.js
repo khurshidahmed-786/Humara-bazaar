eb27f5f29e11852ce0e5a9b3152bb69ec8cc54b6
khurshidahmed-786 <founderhumarabazaar@gmail.com>
2026-07-14 16:21:53 +0530

    cartpage.js

--- js/cartpage.js
+++ js/cartpage.js
@@ -1,1 +1,48 @@
+document.addEventListener(
 
+"DOMContentLoaded",
+
+function(){
+
+renderCart();
+
+});
+function renderCart(){
+
+const app = document.getElementById(
+
+"cartApp"
+
+);
+
+const cart = getCart();
+
+if(cart.length===0){
+
+app.innerHTML=`
+
+<div class="container">
+
+<h1>
+
+🛒 My Cart
+
+</h1>
+
+<br>
+
+<p>
+
+Your cart is empty.
+
+</p>
+
+</div>
+
+`;
+
+return;
+
+}
+
+}
