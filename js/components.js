0f7dcb2c0484fee1e000c1b8539e3c93f594f745
khurshidahmed-786 <founderhumarabazaar@gmail.com>
2026-07-15 12:16:20 +0530

    components.js

--- js/components.js
+++ js/components.js
@@ -1,53 +1,56 @@
-function renderHeader(){
-
-    return `
-
-    <header>
-
-        Header
-
-    </header>
-
-    `;
-
-}
-
-function renderBottomNav(){
-
-    return `
-
-    <nav>
-
-        Bottom Navigation
-
-    </nav>
-
-    `;
-
-}
 function renderHeader(title = "Hamara Bazaar"){
 
     return `
 
-    <header class="appHeader">
+    <header class="topbar">
 
-        <div class="headerLeft">
+        <button
+        id="menuBtn"
+        class="iconBtn">
 
             ☰
 
+        </button>
+
+        <div class="brand">
+
+            <img
+            src="assets/logo icon.png"
+            class="brandLogo"
+            alt="Logo">
+
+            <div>
+
+                <div class="brandName">
+
+                    ${title}
+
+                </div>
+
+                <div class="brandTag">
+
+                    Delivering Happiness
+
+                </div>
+
+                <div class="brandLocation">
+
+                    📍 Surankote Bazaar
+
+                </div>
+
+            </div>
+
         </div>
 
-        <div class="headerTitle">
-
-            ${title}
-
-        </div>
-
-        <div class="headerRight">
+        <button
+        id="cartBtn"
+        class="iconBtn"
+        onclick="location.href='cart.html'">
 
             🛒
 
-        </div>
+        </button>
 
     </header>
 
@@ -55,8 +58,50 @@ function renderHeader(title = "Hamara Bazaar"){
 
 }
 
-function renderBottomNav(){
+function renderBottomNav(current){
 
-    return "";
+    return `
+
+    <nav class="bottomNav">
+
+        <a
+        href="index.html"
+        class="${current=="home"?"active":""}">
+
+            🏠
+            <span>Home</span>
+
+        </a>
+
+        <a
+        href="shops.html"
+        class="${current=="shops"?"active":""}">
+
+            🛍
+            <span>Shops</span>
+
+        </a>
+
+        <a
+        href="sellerorders.html"
+        class="${current=="orders"?"active":""}">
+
+            📦
+            <span>Orders</span>
+
+        </a>
+
+        <a
+        href="profile.html"
+        class="${current=="profile"?"active":""}">
+
+            👤
+            <span>Profile</span>
+
+        </a>
+
+    </nav>
+
+    `;
 
 }
