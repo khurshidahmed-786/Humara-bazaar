ce3b1b48f27a5ba0457c1368378bf55130fa850e
khurshidahmed-786 <founderhumarabazaar@gmail.com>
2026-07-17 17:50:27 +0530

    database.js

--- js/database.js
+++ js/database.js
@@ -162,3 +162,28 @@ function saveOrder(order){
     saveData(DB.ORDERS,orders);
 
 }
+function updateOrderStatus(id,status){
+
+    let orders = getOrders();
+
+    const order = orders.find(
+
+        o => o.id == id
+
+    );
+
+    if(order){
+
+        order.status = status;
+
+    }
+
+    saveData(
+
+        DB.ORDERS,
+
+        orders
+
+    );
+
+}
