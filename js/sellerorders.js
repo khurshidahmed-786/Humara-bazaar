ae39f76ea5202d7ecc9dc9114b884aa64880f13c
khurshidahmed-786 <founderhumarabazaar@gmail.com>
2026-07-17 16:21:53 +0530

    sellerorders.js

--- js/sellerorders.js
+++ js/sellerorders.js
@@ -34,5 +34,133 @@ function renderOrders(){
     }
 const orders = getOrders();
 
-console.log(orders);
+const sellerOrders = orders.filter(
+
+    order => order.shopId == shop.id
+
+);
+
+if(sellerOrders.length===0){
+
+    container.innerHTML=`
+
+    <div class="order">
+
+        <h2>📦 No Orders Yet</h2>
+
+        <p>
+
+            New customer orders will appear here.
+
+        </p>
+
+    </div>
+
+    `;
+
+    return;
+
+}
+
+container.innerHTML="";
+
+sellerOrders.forEach(order=>{
+
+container.innerHTML += `
+
+<div class="order">
+
+<div class="row">
+
+<h2>
+
+Order #${String(order.id).slice(-4)}
+
+</h2>
+
+<div>
+
+${order.status}
+
+</div>
+
+</div>
+
+<br>
+
+<p>
+
+👤 ${order.customerName}
+
+</p>
+
+<p>
+
+📞 ${order.customerPhone}
+
+</p>
+
+<p>
+
+📍 ${order.customerAddress}
+
+</p>
+
+<br>
+
+<p>
+
+💰 Total : ₹${order.total}
+
+</p>
+
+<br>
+
+<a
+class="btn"
+onclick="acceptOrder(${order.id})">
+
+Accept
+
+</a>
+
+<a
+class="btn reject"
+onclick="rejectOrder(${order.id})">
+
+Reject
+
+</a>
+
+</div>
+
+`;
+
+});
+}
+function acceptOrder(id){
+
+updateOrderStatus(
+
+id,
+
+"Accepted"
+
+);
+
+renderOrders();
+
+}
+function rejectOrder(id){
+
+updateOrderStatus(
+
+id,
+
+"Cancelled"
+
+);
+
+renderOrders();
+
 }
