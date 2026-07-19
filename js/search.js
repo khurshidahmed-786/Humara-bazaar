45f7e74828c05ca5b5359058e30d74b80c4cc253
khurshidahmed-786 <founderhumarabazaar@gmail.com>
2026-07-19 11:01:46 +0530

    search.js

--- js/search.js
+++ js/search.js
@@ -76,50 +76,7 @@ Try another keyword.
 
 return;}
 let html = "";
-if(products.length){
 
-    html += `
-
-    <h2 class="sectionTitle">
-
-        Products
-
-    </h2>
-
-    `;
-
-    products.forEach(product=>{
-
-        html += `
-
-        <div class="resultCard">
-
-            <h3>
-
-                ${product.emoji || "📦"} ${product.name}
-
-            </h3>
-
-            <p>
-
-                ₹${product.price}
-
-            </p>
-
-            <button
-            onclick="openProduct(${product.id})">
-
-                View Product
-
-            </button>
-
-        </div>
-
-        `;
-
-    });
-
-}
 if(products.length){
 
 html += `
@@ -226,4 +183,5 @@ Visit
 });
 
 }
+results.innerHTML = html;
 }
