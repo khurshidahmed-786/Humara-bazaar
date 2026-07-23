43bfe7eb715562109ec5cddabad6224eaa4c4d00
khurshidahmed-786 <founderhumarabazaar@gmail.com>
2026-07-23 23:39:07 +0530

    shop.js

--- js/shop.js
+++ js/shop.js
@@ -66,6 +66,42 @@ function addBusinessToUser(
     return true;
 
 }
+function getActiveBusiness(){
+
+    const businessId = Number(
+
+        localStorage.getItem(
+            "hb_activeBusiness"
+        )
+
+    );
+
+    if(!businessId){
+
+        return null;
+
+    }
+
+    return getBusinessById(
+        businessId
+    );
+
+}
+
+
+function getShopByBusinessId(
+    businessId
+){
+
+    return getShops().find(
+
+        shop =>
+
+        shop.businessId == businessId
+
+    ) || null;
+
+}
 /* ===========================
    SHOP MODULE
 =========================== */
